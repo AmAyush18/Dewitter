c76dcdc6656cefc1a7c513b877db893392158bfa
Ayush Dwivedi <91709845+AmAyush18@users.noreply.github.com>
2024-08-03 08:15:42 +0530

    Added deploy script and made some fixes (#3)

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+// scripts/deploy.js
+const hre = require("hardhat");
+
+async function main() {
+  const Dewitter = await hre.ethers.getContractFactory("Dewitter");
+  const dewitter = await Dewitter.deploy();
+
+  await dewitter.waitForDeployment();
+
+  console.log("Dewitter contract deployed to:",await dewitter.getAddress());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
